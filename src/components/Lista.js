a23c06296b6e156c13c8cad079a9c1a4ddc7886d
Brenno Ogeda <brennoogeda@gmail.com>
2019-08-05 22:34:10 -0300

    init

---
+++ src/components/Lista.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const c = () => {
+  return (
+    <ul className="lista">
+        <li>
+            item
+        </li>
+        <li>
+            item
+        </li>
+    </ul>
+  )
+}
+
+export default c
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Lista from '../components/Lista'
+
+const c = () => {
+  return (
+    <>
+      <h1>Brenno</h1>
+      <h1>Brenno</h1> 
+      <Lista></Lista>
+    </>
+  )
+}
+
+export default c
